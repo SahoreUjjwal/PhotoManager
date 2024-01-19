62263292275c3d99df041cde0a3f95c9edf3be1f
ujjwal shaore <sahoreujjwal@gmail.com>
2024-01-19 15:53:19 +0530

    First commit- initial setup

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,8 @@
+
+
+export function NavBar(){
+    return(<div id="container">
+        <img src="https://cdn-icons-png.flaticon.com/512/2659/2659360.png" alt="photo"/>   
+        <span>PhotoFolio</span>
+    </div>)
+}
